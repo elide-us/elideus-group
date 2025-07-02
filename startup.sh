f95816bdc141b2c7b26543f22e03530caa012c8d
Elideus <aaron@elideus.net>
2025-07-01 23:12:07 -0700

    Startup.sh cleanup

--- startup.sh
+++ startup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-set -e
+#set -e
 
 # Start SQL Server in the background
-/opt/mssql/bin/sqlservr &
+#/opt/mssql/bin/sqlservr &
 
 # Give SQL Server some time to start up
-echo "Sleeping 30 seconds to allow SQL Server to initialize..."
-sleep 30
+#echo "Sleeping 30 seconds to allow SQL Server to initialize..."
+#sleep 30
 
 # Start the FastAPI app
-exec python -m uvicorn main:app --host 0.0.0.0 --port 8000
+#exec python -m uvicorn main:app --host 0.0.0.0 --port 8000
+python -m uvicorn main:app --host 0.0.0.0 --port 8000
