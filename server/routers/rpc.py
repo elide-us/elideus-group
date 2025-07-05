70dc3d216f135633436bffa18e86db72b001901c
Elideus <aaron@elideus.net>
2025-07-05 08:57:09 -0700

    fix rpc routing and expose routers

--- server/routers/rpc.py
+++ server/routers/rpc.py
@@ -4,6 +4,7 @@ from rpc.models import RPCRequest, RPCResponse
 
 router = APIRouter()
 
+@router.post("")
 @router.post("/")
 async def post_root(rpc_request: RPCRequest, request: Request) -> RPCResponse:
   return await handle_rpc_request(rpc_request, request)
