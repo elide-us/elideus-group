9a040924bb2c86902e8a9506c2d8682c5fc1cb8a
Elideus <aaron@elideus.net>
2025-07-05 21:56:33 -0700

    Cleanup scripted

---
+++ docker_cleanup.sh
@@ -0,0 +1,13 @@
+set -e
+
+rm -rf /app/tests \
+       /app/scripts \
+       /app/frontend \
+       /app/*.cmd \
+       /app/*.md \
+       /app/*.txt \
+       /app/Dockerfile \
+       /app/.dockerignore \
+       /app/.env.example \
+       /app/.gitignore \
+       /app/.github
