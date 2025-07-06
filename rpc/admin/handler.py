25c73a2ee120bbf3e12d1567ff7a931cad47e314
Elideus <aaron@elideus.net>
2025-07-06 00:01:04 -0700

    Initial scaffolding for Codex

--- rpc/admin/handler.py
+++ rpc/admin/handler.py
@@ -1,9 +1,12 @@
 from fastapi import Request, HTTPException
 from rpc.admin.vars.handler import handle_vars_request
+from rpc.admin.links.handler import handle_links_request
 
 async def handle_admin_request(urn: list[str], request: Request):
   match urn:
     case ["vars", *rest]:
       return await handle_vars_request(rest, request)
+    case ["links", *rest]:
+      return await handle_links_request(rest, request)
     case _:
       raise HTTPException(status_code=404, detail="Unknown RPC subdomain")
