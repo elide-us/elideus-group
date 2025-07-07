c00dd93dde501fbd0047cfec992ccde11549b76e
Elideus <aaron@elideus.net>
2025-07-06 17:29:54 -0700

    Misc updates to provider structure scaffolding

--- server/providers/discord_provider.py
+++ server/providers/discord_provider.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from . import Provider
 
 class DiscordProvider(Provider):
-  def __init__(self, app: FastAPI): super().__init__(app)
-  async def startup(self): pass
-  async def shutdown(self): pass
+  def __init__(self, app: FastAPI):
+    super().__init__(app)
+  async def startup(self):
+    pass
+  async def shutdown(self):
+    pass
