09a51b8b7aee5b346ac3c4104712f302150bbe29
Elideus <aaron@elideus.net>
2025-07-24 19:39:58 -0700

    Adjusted Pagetitle size

--- frontend/src/shared/PageTitle.tsx
+++ frontend/src/shared/PageTitle.tsx
@@ -2,7 +2,7 @@ import { Typography, Divider } from '@mui/material';
 
 export const PageTitle = ({ title }: { title: string }): JSX.Element => (
     <>
-        <Typography variant='h5'>{title}</Typography>
+        <Typography variant='h3'>{title}</Typography>
         <Divider sx={{ mb: 2 }} />
     </>
 );
