9cc1b3058a37f934c7730600f0a1722eb6ac6a5f
Elideus <aaron@elideus.net>
2025-08-07 15:11:18 -0700

    Updated rpc details

--- rpc/account/users/__init__.py
+++ rpc/account/users/__init__.py
@@ -1,6 +1,6 @@
 from .services import (
   account_users_get_profile_v1,
-  account_users_set_display_v1,
+  account_users_reset_display_v1,
   account_users_set_credits_v1, 
   account_users_enable_storage_v1
 )
@@ -13,7 +13,7 @@ DISPATCHERS: dict[tuple[str, str], callable] = {
   """
   ("get_profile", "1"): account_users_get_profile_v1,
   ("set_credits", "1"): account_users_set_credits_v1,
-  ("reset_display", "1"): account_users_set_display_v1,
+  ("reset_display", "1"): account_users_reset_display_v1,
   ("enable_storage", "1"): account_users_enable_storage_v1
 }
 
--- rpc/public/vars/__init__.py
+++ rpc/public/vars/__init__.py
@@ -2,12 +2,15 @@ from .services import (
   public_vars_get_ffmpeg_version_v1,
   public_vars_get_hostname_v1,
   public_vars_get_repo_v1,
-  public_vars_get_version_v1
+  public_vars_get_version_v1,
+  public_vars_get_odbc_version_v1
 )
 
 DISPATCHERS: dict[tuple[str, str], callable] = {
   ("get_version", "1"): public_vars_get_version_v1,
   ("get_hostname", "1"): public_vars_get_hostname_v1,
   ("get_repo", "1"): public_vars_get_repo_v1,
-  ("get_ffmpeg_version", "1"): public_vars_get_ffmpeg_version_v1
+  ("get_ffmpeg_version", "1"): public_vars_get_ffmpeg_version_v1,
+  ("get_odbc_version", "1"): public_vars_get_odbc_version_v1
 }
+
