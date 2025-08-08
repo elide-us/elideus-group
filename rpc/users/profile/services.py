f8362d73b2cb36e66044979e7e58d7eaf2a0abac
Elideus <aaron@elideus.net>
2025-08-07 18:48:55 -0700

    Rename users profile RPC services

--- rpc/users/profile/services.py
+++ rpc/users/profile/services.py
@@ -1,11 +1,11 @@
 from fastapi import Request
 
-async def users_user_get_profile_v1(request: Request):
-  raise NotImplementedError("urn:users:user:get_profile:1")
+async def users_profile_get_profile_v1(request: Request):
+  raise NotImplementedError("urn:users:profile:get_profile:1")
 
-async def users_user_set_display_v1(request: Request):
-  raise NotImplementedError("urn:users:user:set_display:1")
+async def users_profile_set_display_v1(request: Request):
+  raise NotImplementedError("urn:users:profile:set_display:1")
 
-async def users_user_set_optin_v1(request: Request):
-  raise NotImplementedError("urn:users:user:set_optin:1")
+async def users_profile_set_optin_v1(request: Request):
+  raise NotImplementedError("urn:users:profile:set_optin:1")
 
