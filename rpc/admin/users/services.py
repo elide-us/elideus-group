e7a863fc139b9e85fd80958ffa01d3a77330cd57
Elideus <aaron@elideus.net>
2025-08-07 18:08:32 -0700

    Rename account RPCs to admin

--- rpc/admin/users/services.py
+++ rpc/admin/users/services.py
@@ -1,14 +1,14 @@
 from fastapi import Request
 
-async def account_users_get_profile_v1(request: Request):
-  raise NotImplementedError("urn:account:users:get_profile:1")
+async def admin_users_get_profile_v1(request: Request):
+  raise NotImplementedError("urn:admin:users:get_profile:1")
 
-async def account_users_set_credits_v1(request: Request):
-  raise NotImplementedError("urn:account:users:set_credits:1")
+async def admin_users_set_credits_v1(request: Request):
+  raise NotImplementedError("urn:admin:users:set_credits:1")
 
-async def account_users_reset_display_v1(request: Request):
-  raise NotImplementedError("urn:account:users:reset_display:1")
+async def admin_users_reset_display_v1(request: Request):
+  raise NotImplementedError("urn:admin:users:reset_display:1")
 
-async def account_users_enable_storage_v1(request: Request):
-  raise NotImplementedError("urn:account:users:enable_storage:1")
+async def admin_users_enable_storage_v1(request: Request):
+  raise NotImplementedError("urn:admin:users:enable_storage:1")
 
