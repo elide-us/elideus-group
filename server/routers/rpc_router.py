d9145580ae589a8650f94e22723c0c4df4f87065
Elideus <aaron@elideus.net>
2025-08-10 14:58:13 -0700

    Fix RPC router request handling

--- server/routers/rpc_router.py
+++ server/routers/rpc_router.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter, Request
 from rpc.handler import handle_rpc_request
-from rpc.models import RPCRequest, RPCResponse
+from rpc.models import RPCResponse
 
 router = APIRouter()
 
 @router.post("")
 @router.post("/")
-async def post_root(rpc_request: RPCRequest, request: Request) -> RPCResponse:
-  return await handle_rpc_request(rpc_request, request)
+async def post_root(request: Request) -> RPCResponse:
+  return await handle_rpc_request(request)
