d76927dc89e6c5fbcdf69e97e70df27953d0dd55
Elideus <aaron@elideus.net>
2025-08-16 09:10:40 -0700

    Added git pull option

--- dev.cmd
+++ dev.cmd
@@ -12,10 +12,17 @@ IF /I "%CMD%"=="generate" GOTO DO_GENERATE
 IF /I "%CMD%"=="start" GOTO DO_START
 IF /I "%CMD%"=="fast" GOTO DO_FAST
 IF /I "%CMD%"=="test" GOTO DO_TEST
+IF /I "%CMD%"=="restart" GOTO DO_RESTART
 
 :HELP
 ECHO Usage: dev [generate^|start^|fast^|test]
 EXIT /b 1
+
+:GET_LATEST
+ECHO Getting latest from GitHub...
+CALL git pull origin elideus
+IF ERRORLEVEL 1 EXIT /b 1
+EXIT /b 0
 
 :INSTALL_DEPS
 ECHO Installing Python dependencies...
@@ -61,6 +68,7 @@ IF ERRORLEVEL 1 EXIT /b 1
 EXIT /b 0
 
 :DO_GENERATE
+CALL :GET_LATEST
 CALL :GENERATE_LIBS
 EXIT /b %ERRORLEVEL%
 
@@ -81,9 +89,19 @@ CALL python -m uvicorn main:app --reload --host localhost
 EXIT /b %ERRORLEVEL%
 
 :DO_TEST
+CALL :GET_LATEST
 CALL :INSTALL_DEPS || EXIT /b 1
 CALL :GENERATE_LIBS || EXIT /b 1
 CALL :FRONTEND_TASKS || EXIT /b 1
 CALL :RUN_PYTEST
 EXIT /b %ERRORLEVEL%
 
+:DO_RESTART
+CALL :GET_LATEST || EXIT /b 1
+CALL :INSTALL_DEPS || EXIT /b 1
+CALL :GENERATE_LIBS || EXIT /b 1
+CALL :FRONTEND_TASKS || EXIT /b 1
+CALL :RUN_PYTEST || EXIT /b 1
+ECHO Starting Uvicorn...
+CALL python -m uvicorn main:app --reload --host localhost
+EXIT /b %ERRORLEVEL%
