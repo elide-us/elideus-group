b79adce057fac1c107e4404289b58217df65fa98
Aaron Stackpole <aaron@elideus.net>
2025-08-21 20:10:21 -0700

    Removed the weird injected clientid lookup...

--- frontend/src/config/google.ts
+++ frontend/src/config/google.ts
@@ -1,7 +1,5 @@
 export const googleConfig = {
-	clientId:
-		import.meta.env.VITE_GOOGLE_CLIENT_ID ||
-		'295304659309-vkbjt5572fg3vjlqbj3qkkfgal83pcrj.apps.googleusercontent.com',
+	clientId: '295304659309-vkbjt5572fg3vjlqbj3qkkfgal83pcrj.apps.googleusercontent.com',
 	scope: 'openid profile email',
 };
 export default googleConfig;
