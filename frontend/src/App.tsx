b7adc187ecb6fb53d06a737a9c9e9dd50232cf51
Elideus <aaron@elideus.net>
2025-08-28 20:34:30 -0700

    refactor: use support roles for account user listing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,8 @@ const SystemRolesPage = lazy(() => import("./pages/system/SystemRolesPage"));
 const FileManager = lazy(() => import("./pages/FileManager"));
 const SystemConfigPage = lazy(() => import("./pages/system/SystemConfigPage"));
 const AccountRolesPage = lazy(() => import("./pages/AccountRolesPage"));
+const AccountUsersPage = lazy(() => import("./pages/AccountUsersPage"));
+const AccountUserPanel = lazy(() => import("./pages/AccountUserPanel"));
 
 function App(): JSX.Element {
 	return (
@@ -21,32 +23,34 @@ function App(): JSX.Element {
 			<CssBaseline />
 			<UserContextProvider>
 				<Router>
-                                        <NavBar />
-                                        <Container
-                                                maxWidth="lg"
-                                                disableGutters
-                                                sx={{
-                                                        bgcolor: "background.paper",
-                                                        color: "text.primary",
-                                                        minHeight: "100vh",
-                                                        py: 2,
-                                                }}
-                                        >
-                                                <Suspense fallback={<div>Loading...</div>}>
-                                                        <Routes>
-                                                                <Route path="/" element={<Home />} />
-                                                                <Route path="/gallery" element={<Gallery />} />
-                                                                <Route path="/loginpage" element={<LoginPage />} />
-                                                                <Route path="/userpage" element={<UserPage />} />
-                                                                <Route path="/system-routes" element={<SystemRoutesPage />} />
-                                                                <Route path="/system-config" element={<SystemConfigPage />} />
-                                                                <Route path="/system-roles" element={<SystemRolesPage />} />
-                                                                <Route path="/account-roles" element={<AccountRolesPage />} />
-                                                                <Route path="/file-manager" element={<FileManager />} />
-                                                        </Routes>
-                                                </Suspense>
-                                        </Container>
-                                </Router>
+					<NavBar />
+					<Container
+						maxWidth="lg"
+						disableGutters
+						sx={{
+							bgcolor: "background.paper",
+							color: "text.primary",
+							minHeight: "100vh",
+							py: 2,
+						}}
+					>
+						<Suspense fallback={<div>Loading...</div>}>
+							<Routes>
+								<Route path="/" element={<Home />} />
+								<Route path="/gallery" element={<Gallery />} />
+								<Route path="/loginpage" element={<LoginPage />} />
+								<Route path="/userpage" element={<UserPage />} />
+								<Route path="/system-routes" element={<SystemRoutesPage />} />
+								<Route path="/system-config" element={<SystemConfigPage />} />
+								<Route path="/system-roles" element={<SystemRolesPage />} />
+								<Route path="/account-roles" element={<AccountRolesPage />} />
+								<Route path="/account-users" element={<AccountUsersPage />} />
+								<Route path="/account-users/:guid" element={<AccountUserPanel />} />
+								<Route path="/file-manager" element={<FileManager />} />
+							</Routes>
+						</Suspense>
+					</Container>
+				</Router>
 			</UserContextProvider>
 		</ThemeProvider>
 	);
