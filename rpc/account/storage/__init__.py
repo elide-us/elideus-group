0546b01a1ee6943e7eab6b0e1fda347c4f9b54d8
Elideus <aaron@elideus.net>
2025-09-07 20:16:54 -0700

    refactor: remove account storage provision RPC

--- rpc/account/storage/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Account storage namespace for storage operations requiring account admin."""
-
-from .provision.handler import handle_provision_request
-
-HANDLERS: dict[str, callable] = {
-  "provision": handle_provision_request,
-}
--- rpc/account/storage/provision/models.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-
-class StorageProvisionStatus1(BaseModel):
-  exists: bool
