1a1e55e9b5e416c976b4755b48b94fb6fb57b23f
Elideus <aaron@elideus.net>
2025-09-13 15:19:02 -0700

    Updates to OAuth configs

--- frontend/src/config/discord.ts
+++ frontend/src/config/discord.ts
@@ -1,7 +1,7 @@
 export const discordConfig = {
-        clientId: '000000000000000000',
-        scope: 'identify email',
-        redirectUri: `${window.location.origin}/userpage`,
+        clientId: '1339726954986999878',
+        scope: 'identify email openid',
+        redirectUri: `${window.location.origin}`,
         authorizeEndpoint: 'https://discord.com/oauth2/authorize',
 };
 export default discordConfig;
