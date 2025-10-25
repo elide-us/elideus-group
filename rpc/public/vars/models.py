0dfe0171e4daea6217820ad5ef69a2f7d0b3718b
Elideus <aaron@elideus.net>
2025-10-25 14:22:32 -0700

    Consolidate public vars RPC to versions endpoint

--- rpc/public/vars/models.py
+++ rpc/public/vars/models.py
@@ -1,26 +1,6 @@
 from pydantic import BaseModel
 
 
-class PublicVarsVersion1(BaseModel):
-  version: str
-
-
-class PublicVarsHostname1(BaseModel):
-  hostname: str
-
-
-class PublicVarsRepo1(BaseModel):
-  repo: str
-
-
-class PublicVarsFfmpegVersion1(BaseModel):
-  ffmpeg_version: str
-
-
-class PublicVarsOdbcVersion1(BaseModel):
-  odbc_version: str
-
-
 class PublicVarsVersions1(BaseModel):
   hostname: str
   version: str
