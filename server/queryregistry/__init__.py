769b4ce03374321f76918f54349b6f3dbd19deeb
Elideus <aaron@elideus.net>
2025-11-01 16:23:45 -0700

    Remove query registry public namespace

--- server/queryregistry/__init__.py
+++ server/queryregistry/__init__.py
@@ -6,13 +6,11 @@ Routes query operations to domain-specific handlers.
 from .account.handler import handle_account_request
 from .finance.handler import handle_finance_request
 from .handler import dispatch_query_request
-from .public.handler import handle_public_request
 from .system.handler import handle_system_request
 
 HANDLERS: dict[str, callable] = {
   "account": handle_account_request,
   "finance": handle_finance_request,
-  "public": handle_public_request,
   "system": handle_system_request,
 }
 
