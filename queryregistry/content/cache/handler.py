1bdb379758e1bc4f0f102dce9bc139b1b49a7fde
Elideus <aaron@elideus.net>
2026-02-26 12:43:11 -0800

    Migrate content cache registry operations to queryregistry

--- queryregistry/content/cache/handler.py
+++ queryregistry/content/cache/handler.py
@@ -6,11 +6,38 @@ from typing import Sequence
 
 from queryregistry.dispatch import dispatch_subdomain_request
 from queryregistry.models import DBRequest, DBResponse
-from queryregistry.stubs import build_stub_dispatchers
+
+from .services import (
+  count_rows_v1,
+  delete_folder_v1,
+  delete_v1,
+  get_published_files_v1,
+  list_public_v1,
+  list_reported_v1,
+  list_v1,
+  replace_user_v1,
+  set_gallery_v1,
+  set_public_v1,
+  set_reported_v1,
+  upsert_v1,
+)
 
 __all__ = ["handle_cache_request"]
 
-DISPATCHERS = build_stub_dispatchers("content.cache")
+DISPATCHERS = {
+  ("list", "1"): list_v1,
+  ("list_public", "1"): list_public_v1,
+  ("list_reported", "1"): list_reported_v1,
+  ("replace_user", "1"): replace_user_v1,
+  ("upsert", "1"): upsert_v1,
+  ("delete", "1"): delete_v1,
+  ("delete_folder", "1"): delete_folder_v1,
+  ("set_public", "1"): set_public_v1,
+  ("set_reported", "1"): set_reported_v1,
+  ("count_rows", "1"): count_rows_v1,
+  ("set_gallery", "1"): set_gallery_v1,
+  ("get_published_files", "1"): get_published_files_v1,
+}
 
 
 async def handle_cache_request(
