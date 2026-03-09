f7b4226f7e1c2ebf2b4287364d862b56c64452da
Elideus <aaron@elideus.net>
2026-03-08 18:29:23 -0700

    Rewire module registry imports to queryregistry domains

--- server/modules/bsky_module.py
+++ server/modules/bsky_module.py
@@ -7,8 +7,8 @@ from dataclasses import dataclass
 from atproto import AsyncClient as AsyncBskyClient, client_utils
 from fastapi import FastAPI
 
-from server.registry.system.config.model import ConfigKeyParams
-from server.modules.registry.helpers import get_config_request
+from queryregistry.system.config.models import ConfigKeyParams
+from queryregistry.system.config import get_config_request
 from . import BaseModule
 from .db_module import DbModule
 
