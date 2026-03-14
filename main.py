57ec6c8de5ef225fd4cef831937a165342c5d7aa
Elideus <aaron@elideus.net>
2026-03-14 14:39:23 -0700

    Refactor MCP endpoint to native FastAPI route

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from server.helpers.logging import configure_root_logging
 from server.lifespan import lifespan
-from server.mcp_server import get_mcp_app, set_gateway_resolver
-from server.routers import oauth_router, rpc_router, web_router
+from server.mcp_server import init_session_manager, set_gateway_resolver
+from server.routers import mcp_router, oauth_router, rpc_router, web_router
 
 configure_root_logging(4)
 
@@ -14,9 +14,8 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(rpc_router.router, prefix="/rpc")
 app.include_router(oauth_router.router)
 set_gateway_resolver(lambda: app.state.mcp_gateway)
-_mcp_app = get_mcp_app()
-if _mcp_app is not None:
-  app.mount("/mcp", _mcp_app)
+init_session_manager()
+app.include_router(mcp_router.router)
 app.include_router(web_router.router)
 
 @app.get("/")
