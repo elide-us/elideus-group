0eb2f246f10d288fd240eb2ce355a447cb8d6d39
Elideus <aaron@elideus.net>
2026-03-15 14:02:09 -0700

    Add system batch jobs RPC namespace handlers

--- rpc/system/__init__.py
+++ rpc/system/__init__.py
@@ -3,6 +3,7 @@
 Requires ROLE_SYSTEM_ADMIN.
 """
 
+from .batch_jobs.handler import handle_batch_jobs_request
 from .config.handler import handle_config_request
 from .conversations.handler import handle_conversations_request
 from .models.handler import handle_models_request
@@ -10,6 +11,7 @@ from .roles.handler import handle_roles_request
 from .storage.handler import handle_storage_request
 
 HANDLERS: dict[str, callable] = {
+  "batch_jobs": handle_batch_jobs_request,
   "config": handle_config_request,
   "conversations": handle_conversations_request,
   "models": handle_models_request,
