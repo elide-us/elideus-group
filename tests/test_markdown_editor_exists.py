77eb395b84dd0443f60941a8eab389300730dbff
Aaron Stackpole <aaron@elideus.net>
2026-03-29 21:28:43 -0700

    Removing stale tests before refactor

--- tests/test_markdown_editor_exists.py
+++
@@ -1,6 +0,0 @@
-import os
-
-
-def test_markdown_editor_component_exists():
-  path = os.path.join("frontend", "src", "components", "MarkdownEditor.tsx")
-  assert os.path.isfile(path), f"MarkdownEditor.tsx not found at {path}"
