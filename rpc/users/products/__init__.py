bd7249d8fcda83aaf2abc29cb976b7d31e91978d
Elideus <aaron@elideus.net>
2026-04-05 09:39:36 -0700

    Remove users products RPC subdomain and frontend ProductsPage

--- rpc/users/products/__init__.py
+++
@@ -1,7 +0,0 @@
-from .services import users_products_list_v1, users_products_purchase_v1
-
-
-DISPATCHERS = {
-  ("list", "1"): users_products_list_v1,
-  ("purchase", "1"): users_products_purchase_v1,
-}
