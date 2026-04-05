83f00d241ad3b016aa3b99de1eb38170f6dc2496
Elideus <aaron@elideus.net>
2026-04-04 21:24:28 -0700

    Remove system config page and obsolete RPC subdomains

--- rpc/system/__init__.py
+++ rpc/system/__init__.py
@@ -3,20 +3,16 @@
 Requires ROLE_SYSTEM_ADMIN.
 """
 
-from .config.handler import handle_config_request
 from .conversations.handler import handle_conversations_request
 from .models.handler import handle_models_request
 from .roles.handler import handle_roles_request
 from .scheduled_tasks.handler import handle_scheduled_tasks_request
-from .storage.handler import handle_storage_request
 from .workflows.handler import handle_workflows_request
 
 HANDLERS: dict[str, callable] = {
-  "config": handle_config_request,
   "conversations": handle_conversations_request,
   "models": handle_models_request,
   "roles": handle_roles_request,
   "scheduled_tasks": handle_scheduled_tasks_request,
-  "storage": handle_storage_request,
   "workflows": handle_workflows_request,
 }
