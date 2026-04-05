66e42d894426acf42e2af30be0ce73457d711387
Elideus <aaron@elideus.net>
2026-04-05 11:40:28 -0700

    Remove service renewals page and RPC subdomains

--- rpc/service/__init__.py
+++ rpc/service/__init__.py
@@ -6,8 +6,6 @@ Requires ROLE_SERVICE_ADMIN.
 from .roles.handler import handle_roles_request
 from .routes.handler import handle_routes_request
 from .reflection.handler import handle_reflection_request
-from .renewals.handler import handle_renewals_request
-from .payment_requests.handler import handle_payment_requests_request
 from .pages.handler import handle_pages_request
 from .rpcdispatch.handler import handle_rpcdispatch_request
 
@@ -15,8 +13,6 @@ HANDLERS: dict[str, callable] = {
   "roles": handle_roles_request,
   "routes": handle_routes_request,
   "reflection": handle_reflection_request,
-  "renewals": handle_renewals_request,
-  "payment_requests": handle_payment_requests_request,
   "pages": handle_pages_request,
   "rpcdispatch": handle_rpcdispatch_request,
 }
