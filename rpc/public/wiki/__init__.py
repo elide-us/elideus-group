d7cd4763097e6e0e9b9bf75fda729c5f219f14fd
Elideus <aaron@elideus.net>
2026-04-06 12:16:38 -0700

    Remove wiki RPC subdomains and frontend WikiPage

--- rpc/public/wiki/__init__.py
+++
@@ -1,6 +0,0 @@
-from .services import public_wiki_get_page_v1, public_wiki_list_pages_v1
-
-DISPATCHERS: dict[tuple[str, str], callable] = {
-  ("list_pages", "1"): public_wiki_list_pages_v1,
-  ("get_page", "1"): public_wiki_get_page_v1,
-}
--- rpc/users/__init__.py
+++ rpc/users/__init__.py
@@ -4,11 +4,8 @@ All subdomains require ROLE_REGISTERED.
 """
 
 from .pages.handler import handle_pages_request
-from .wiki.handler import handle_wiki_request
 
 
 HANDLERS: dict[str, callable] = {
-  "pages": handle_pages_request,
-  "wiki": handle_wiki_request
+  "pages": handle_pages_request
 }
-
