6c6d00bb118c64bc70a26e1587a57b7f6d65f0ed
Elideus <aaron@elideus.net>
2026-04-06 20:04:20 -0700

    Remove service roles page and RPC subdomain

--- rpc/service/__init__.py
+++ rpc/service/__init__.py
@@ -3,13 +3,11 @@
 Requires ROLE_SERVICE_ADMIN.
 """
 
-from .roles.handler import handle_roles_request
 from .routes.handler import handle_routes_request
 from .reflection.handler import handle_reflection_request
 from .rpcdispatch.handler import handle_rpcdispatch_request
 
 HANDLERS: dict[str, callable] = {
-  "roles": handle_roles_request,
   "routes": handle_routes_request,
   "reflection": handle_reflection_request,
   "rpcdispatch": handle_rpcdispatch_request,
