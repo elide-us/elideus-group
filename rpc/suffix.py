702d712232d6f49474452745baa3107946203e8f
Aaron Stackpole <aaron@elideus.net>
2026-04-08 19:48:19 -0700

    Couple more cleanups

--- rpc/suffix.py
+++
@@ -1,7 +0,0 @@
-"""RPC response suffix handling placeholder.
-
-This module previously parsed URN suffix tokens (such as view parameters)
-and applied registered suffix handlers to RPC responses. The functionality
-has been removed pending a future redesign.
-"""
-
