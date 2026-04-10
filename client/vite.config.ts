1987a0501ca4cef79a7d604c3ae011f2c4a36cf2
Aaron Stackpole <aaron@elideus.net>
2026-04-10 12:19:44 -0700

    The actual vite config LOL

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,37 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-	plugins: [react()],
-	server: {
-		proxy: {
-			'/rpc': {
-				target: 'http://localhost:8000',
-				changeOrigin: true,
+export default defineConfig(({ mode }) => {
+	const isProduction = mode === 'production';
+
+	return {
+		plugins: [react()],
+		base: isProduction ? '/static/' : '/',
+		build: {
+			outDir: '../static',
+			assetsDir: 'assets',
+			rollupOptions: {
+				output: {
+					manualChunks(id) {
+						if (id.includes('node_modules')) {
+							return 'vendor';
+						}
+						if (id.includes('src/shared/theme/')) {
+							return 'theme';
+						}
+						const match = id.match(/src\/pages\/(system|service|admin|finance)\//);
+						if (match) {
+							return match[1];
+						}
+					},
+				},
 			},
 		},
-	},
+		test: {
+			coverage: {
+				reporter: ['text', 'json', 'html'],
+				exclude: ['src/rpc/**', 'src/shared/RpcModels.tsx'],
+			},
+		},
+	};
 });
