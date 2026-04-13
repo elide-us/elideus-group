b019741c704fe17edeed6ec8af41efbcc338b2b2
Elideus <aaron@elideus.net>
2026-04-13 09:40:57 -0700

    Add dynamic object builder dispatch via builderComponent

--- client/src/engine/registry.ts
+++ client/src/engine/registry.ts
@@ -1,6 +1,7 @@
 import type { ComponentType } from 'react';
 
 import { DevModeToggle } from '../components/DevModeToggle';
+import { DatabaseBuilder } from '../components/DatabaseBuilder';
 import { ContentPanel } from '../components/ContentPanel';
 import { HamburgerToggle } from '../components/HamburgerToggle';
 import { ImageElement } from '../components/ImageElement';
@@ -10,12 +11,14 @@ import { LinkButton } from '../components/LinkButton';
 import { NavigationTreeView } from '../components/NavigationTreeView';
 import { ObjectEditor } from '../components/ObjectEditor';
 import { ObjectTreeView } from '../components/ObjectTreeView';
+import { ModulesBuilder } from '../components/ModulesBuilder';
 import { NavigationSidebar } from '../components/NavigationSidebar';
 import { SidebarContent } from '../components/SidebarContent';
 import { SidebarFooter } from '../components/SidebarFooter';
 import { SidebarHeader } from '../components/SidebarHeader';
 import { SimplePage } from '../components/SimplePage';
 import { StringControl } from '../components/StringControl';
+import { TypesBuilder } from '../components/TypesBuilder';
 import { UserProfileControl } from '../components/UserProfileControl';
 import { Workbench } from '../components/Workbench';
 
@@ -40,4 +43,7 @@ export const COMPONENT_REGISTRY: Record<string, ComponentType<CmsComponentProps>
 	LoginControl,
 	UserProfileControl,
 	StringControl,
+	DatabaseBuilder: DatabaseBuilder as unknown as ComponentType<CmsComponentProps>,
+	TypesBuilder: TypesBuilder as unknown as ComponentType<CmsComponentProps>,
+	ModulesBuilder: ModulesBuilder as unknown as ComponentType<CmsComponentProps>,
 };
