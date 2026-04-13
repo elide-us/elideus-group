a8c3e0811de83395336c6707208dc6186ac162b6
Elideus <aaron@elideus.net>
2026-04-12 22:37:28 -0700

    Add dev-mode navigation sidebar width

--- client/src/components/NavigationSidebar.tsx
+++ client/src/components/NavigationSidebar.tsx
@@ -5,13 +5,15 @@ import { LAYOUT } from '../theme/layoutConstants';
 
 export function NavigationSidebar({ data, children }: CmsComponentProps): JSX.Element {
 	const isOpen = data.__sidebarOpen === true;
+	const isDevMode = data.__devMode === true;
+	const expandedWidth = isDevMode ? LAYOUT.NAV_WIDTH_DEV : LAYOUT.NAV_WIDTH_EXPANDED;
 
 	return (
 		<Box
 			sx={{
 				display: 'flex',
 				flexDirection: 'column',
-				width: isOpen ? LAYOUT.NAV_WIDTH_EXPANDED : LAYOUT.NAV_WIDTH_COLLAPSED,
+				width: isOpen ? expandedWidth : LAYOUT.NAV_WIDTH_COLLAPSED,
 				minHeight: '100vh',
 				bgcolor: '#000000',
 				color: '#FFFFFF',
